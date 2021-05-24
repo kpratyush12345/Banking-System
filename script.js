1a92bce3cf2c21d0f1eec87afde836b93f56ef8c
Pratyush Kumar <66811279+kpratyush12345@users.noreply.github.com>
2021-05-24 10:56:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@
 // DIFFERENT DATA! Contains movement dates, currency and locale
 
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Pratyush Kumar',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
-  pin: 1111,
+  pin: 1234,
 
   movementsDates: [
     '2019-11-18T21:31:17.178Z',
@@ -25,15 +25,15 @@ const account1 = {
     '2020-07-28T23:36:17.929Z',
     '2020-08-01T10:51:36.790Z',
   ],
-  currency: 'EUR',
-  locale: 'pt-PT', // de-DE
+  currency: 'INR',
+  locale: 'en-US', // de-DE
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Sundar Pichai',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
-  pin: 2222,
+  pin: 6789,
 
   movementsDates: [
     '2019-11-01T13:15:33.035Z',
